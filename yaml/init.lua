95727f8473108a2d5767507b5191d7de34edc166
Gaspard Bucher <gaspard@teti.ch>
2014-01-29 18:38:22 +0100

    Added Kirill in copyright line.

--- yaml/init.lua
+++ yaml/init.lua
@@ -7,7 +7,7 @@
 
   <html><a href="https://github.com/lubyk/yaml"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a></html>
 
-  *MIT license* &copy; Andrew Danforth 2014.
+  *MIT license* &copy; Kirill Siminov 2006, Andrew Danforth 2009.
 
   ## Installation
   
